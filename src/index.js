1148b1895cf7f9c8bd7912d2ae5250b41d6d348f
Wang Minan <wangminan0811@hotmail.com>
2024-06-13 16:41:13 +0000

    change domain to mine

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.wangminan.me": "https://registry-1.docker.io",
+  "quay.wangminan.me": "https://quay.io",
+  "gcr.wangminan.me": "https://gcr.io",
+  "k8s-gcr.wangminan.me": "https://k8s.gcr.io",
+  "k8s.wangminan.me": "https://registry.k8s.io",
+  "ghcr.wangminan.me": "https://ghcr.io",
+  "cloudsmith.wangminan.me": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
